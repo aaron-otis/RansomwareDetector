21718338d586e0f777e4c8397d72c3ceb45ef845
aron <amotis@calpoly.edu>
2019-05-20 15:07:56 -0700

    Moved most of the logic into a main function. Added support for connecting to a database and parsing a server string. Added an option to only collect statistics.

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 import argparse
-import cle
 import os
 from analysis import Sample
 from results import Result, print_statistics
 
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser(description="Attempts to discover ransomeware")
-    ap.add_argument("path", nargs="+", help="A file or path to analyze")
-    ap.add_argument("--load_libs", action="store_true", default=False,
-                    help="Toggle automatic loading of linked libraries")
-    ap.add_argument("-c", "--cfg", action="store_true", default=False,
-                    help="Display control flow graph (in a very unorganized way)")
-    ap.add_argument("-C", "--callgraph", action="store_true", default=False,
-                    help="Display call graph")
-    ap.add_argument("-r", "--recursive", action="store_true", default=False,
-                    help="Recursively process the given path")
-    ap.add_argument("-d", "--depth", type=int, default=1,
-                    help="Recursion depth")
-    ap.add_argument("-v", "--verbose", action="store_true", default=False,
-                    help="Be more verbose")
-    args = ap.parse_args()
 
-    result = Result()
+def main(args):
+    result = Result(db=parse_server_info(args.server))
     num_files = 0
     sample = None
     stats = None
@@ -81,3 +65,43 @@ if __name__ == "__main__":
 
     for k, v in result.collect_statistics().items():
         print("{}: {}".format(k.replace("_", " "), v))
+
+
+def parse_server_info(server):
+    """
+    Parses a server string. Currently supports 'server:port' or 'server'.
+    Returns a dictionary suitable for use in a Result object if server is a
+    nonempty string, or None otherwise.
+    """
+    db_info = None
+    if server:
+        if ":" in server:
+            db_info = server.split(":")
+            db_info = {"server": db_info[0], "port": db_info[1]}
+        else:
+            db_info = {"server": server}
+
+    return db_info
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(description="Attempts to discover ransomeware")
+    ap.add_argument("path", nargs="+", help="A file or path to analyze")
+    ap.add_argument("-c", "--cfg", action="store_true", default=False,
+                    help="Display control flow graph (in a very unorganized way)")
+    ap.add_argument("-C", "--callgraph", action="store_true", default=False,
+                    help="Display call graph")
+    ap.add_argument("-r", "--recursive", action="store_true", default=False,
+                    help="Recursively process the given path")
+    ap.add_argument("-d", "--depth", type=int, default=1,
+                    help="Recursion depth")
+    ap.add_argument("-v", "--verbose", action="store_true", default=False,
+                    help="Be more verbose")
+    ap.add_argument("--server", default="", help="The database server to connect to, either 'server:port' or 'server'")
+    ap.add_argument("--statistics", action="store_true", default=False,
+                    help="Only gather statistics, do not perform analysis")
+    ap.add_argument("--load_libs", action="store_true", default=False,
+                    help="Toggle automatic loading of linked libraries")
+    args = ap.parse_args()
+
+    main(args)
